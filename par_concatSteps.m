84ff1e14a06db1995b1295479bc6043bfd0561cb
neil <neil.banas@strath.ac.uk>
2017-03-22 15:58:47 +0000

    fixed saving to files; added ability to read them back in with par_concatSteps

--- par_concatSteps.m
+++ par_concatSteps.m
@@ -1,22 +1,49 @@
-function P = par_concatSteps(steps);
+function P = par_concatSteps(A,fields);
 
 % P = par_concatSteps(steps);
 % P = par_concatSteps(filebasename);
+%                              ..., whichFields);
 %
 % concatenates steps variable by variable into a single structure P that's 
 % easier to analyze.
 % either takes the steps from a vector of structures, or from a sequence of
 % files filebasename1.mat, filebasename2.mat,...
-%
-% TODO: the file-based version!
 
-N = length(steps);
-fields = fieldnames(steps(1));
-for i=1:length(fields)
-	P.(fields{i}) = nan.*ones([N size(steps(1).(fields{i}))]);
-end
-for n=1:N
-	for i=1:length(fields)
-		P.(fields{i})(n,:) = steps(n).(fields{i})(:);
+if isstruct(A) % series of steps stored in a variable --------------------------
+	N = length(A);
+	if nargin < 2
+		fields = fieldnames(A(1));
 	end
+	for i=1:length(fields)
+		P.(fields{i}) = nan.*ones([N size(A(1).(fields{i}))]);
+	end
+	for n=1:N
+		for i=1:length(fields)
+			P.(fields{i})(n,:) = A(n).(fields{i})(:);
+		end
+	end
+	
+elseif ischar(A) % series of files ---------------------------------------------
+	thefiles = dir([A '*.mat']);
+	filenames = {thefiles.name};
+	if isempty(filenames)
+		warning(['no files found at ' A '*.mat']);
+	end
+	N = length(filenames);
+	load(filenames{1},'step');
+	if nargin < 2
+		fields = fieldnames(step);
+	end
+	for i=1:length(fields)
+		P.(fields{i}) = nan.*ones([N size(step.(fields{i}))]);
+	end	
+	for n=1:N
+		load(filenames{n},'step');
+		for i=1:length(fields)
+			P.(fields{i})(n,:) = step.(fields{i})(:);
+		end
+	end	
+	
+else
+	error('neither a structure nor a string.');
 end
